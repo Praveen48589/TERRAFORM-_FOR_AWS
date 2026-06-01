99d48a3479f245dfff79207052058d314b9e9cd0
PRAVEEN SINGH TOMAR <tomarpraveen640@gmail.com>
2026-06-01 11:11:08 +0530

    Update main.tf

--- Project-1-ec2/main.tf
+++ Project-1-ec2/main.tf
@@ -11,10 +11,16 @@ provider "aws" {
     region = var.region
 }
 
+resource "aws_key_pair" "terra-key" {
+    key_name = "terra-key"
+    public_key = file("terra-key.pub")  # paste you public key file name here you can be create by boths keys by using ssh-keygen , name >> terra-key 
+}
+
 
 resource "aws_instance" "my-server" {
     ami = var.ami
     instance_type = var.type
+    key_name = aws_key_pair.terra-key.key_name
 
     vpc_security_group_ids = [
         aws_security_group.sg-group.id
